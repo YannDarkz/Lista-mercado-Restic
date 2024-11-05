68df17447abfcc0e78d122fd177ca0e2d0016caa
yanndarkz <iansantosmcqueen@gmail.com>
2024-11-05 14:24:58 -0300

    feature appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,6 @@ import { AuthService } from '@auth0/auth0-angular';
 import { RouterOutlet, Router } from '@angular/router';
 import { UserService } from './services/user/user.service';
 
-
-// import { ListItemsComponent } from './components/list-items/list-items.component';
-// import { BuyItemComponent } from './components/buy-item/buy-item.component';
 import { CommonModule } from '@angular/common';
 import { AuthButtonsComponent } from './components/auth-buttons/auth-buttons.component';
 import { UserDataService } from './services/user-data/user-data.service';
@@ -13,13 +10,13 @@ import { UserDataService } from './services/user-data/user-data.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,  CommonModule, AuthButtonsComponent],
+  imports: [RouterOutlet, CommonModule, AuthButtonsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
 
-  constructor(public auth: AuthService, private router: Router, private userService: UserService, private userDataService: UserDataService) {}
+  constructor(public auth: AuthService, private router: Router, private userService: UserService, private userDataService: UserDataService) { }
 
   ngOnInit() {
     this.auth.isAuthenticated$.subscribe((isAuthenticated) => {
@@ -27,14 +24,18 @@ export class AppComponent implements OnInit {
         this.router.navigate(['/listItems']);
 
         this.auth.user$.subscribe(user => {
+          console.log("uzi", user);
           
-          if(user) {
+          if (user) {
             const userData = {
+              id: user.sub ? user.sub.split('|')[1] : '',
               name: user.name || 'Usuário sem nome',
               email: user.email || '',
               picture: user.picture || ''
             };
-          
+            console.log("datap", userData);
+            
+
             this.userService.saveAutorizedUser(userData).subscribe(savedUser => {
               this.userDataService.setUserData(savedUser);
             });
@@ -43,36 +44,4 @@ export class AppComponent implements OnInit {
       }
     });
   }
-
-  removeTextNotify = ''
-  addTextNotify = '';
-  
-  notifyAddItem(): void {
-    this.addTextNotify = 'adcionado com sucesso! ✅'
-    setTimeout(() => {
-      this.addTextNotify = ''
-    },2000)
-  }
-
-  notifyEditItem(): void {
-    this.addTextNotify = 'Editado com sucesso! ✅'
-    setTimeout(() => {
-      this.addTextNotify = ''
-    },2000)
-  }
-
-  notifyRemoveItem(): void {
-    this.addTextNotify = 'Removido com sucesso! ✅'
-    setTimeout(() => {
-      this.addTextNotify = ''
-    },1000)
-  }
-
-  notifyAddBuyItem(): void {
-    this.addTextNotify = 'adcionado No carrinho! ✅'
-    setTimeout(() => {
-      this.addTextNotify = ''
-    },1500)
-  }
-
 }
