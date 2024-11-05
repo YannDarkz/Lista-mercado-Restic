2b787cc24d7c5ed34d4b116346fecdbe8049b9c3
yanndarkz <iansantosmcqueen@gmail.com>
2024-11-05 14:26:49 -0300

    created component home

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { AuthButtonsComponent } from '../auth-buttons/auth-buttons.component';
+
+
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [AuthButtonsComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
