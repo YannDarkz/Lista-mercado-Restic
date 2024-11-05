781a8e6a90e849f19b600f2339c23b2d825a5430
yanndarkz <iansantosmcqueen@gmail.com>
2024-11-04 23:27:22 -0300

    feature appComponent.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,52 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { RouterOutlet, Router } from '@angular/router';
+import { UserService } from './services/user/user.service';
 
-import { ListItemsComponent } from './components/list-items/list-items.component';
-import { BuyItemComponent } from './components/buy-item/buy-item.component';
+
+// import { ListItemsComponent } from './components/list-items/list-items.component';
+// import { BuyItemComponent } from './components/buy-item/buy-item.component';
 import { CommonModule } from '@angular/common';
+import { AuthButtonsComponent } from './components/auth-buttons/auth-buttons.component';
+import { UserDataService } from './services/user-data/user-data.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BuyItemComponent, ListItemsComponent, CommonModule],
+  imports: [RouterOutlet,  CommonModule, AuthButtonsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(public auth: AuthService, private router: Router, private userService: UserService, private userDataService: UserDataService) {}
+
+  ngOnInit() {
+    this.auth.isAuthenticated$.subscribe((isAuthenticated) => {
+      if (isAuthenticated) {
+        this.router.navigate(['/listItems']);
+
+        this.auth.user$.subscribe(user => {
+          
+          if(user) {
+            const userData = {
+              name: user.name || 'Usuário sem nome',
+              email: user.email || '',
+              picture: user.picture || ''
+            };
+          
+            this.userService.saveAutorizedUser(userData).subscribe(savedUser => {
+              this.userDataService.setUserData(savedUser);
+            });
+          }
+        })
+      }
+    });
+  }
+
   removeTextNotify = ''
   addTextNotify = '';
-
+  
   notifyAddItem(): void {
     this.addTextNotify = 'adcionado com sucesso! ✅'
     setTimeout(() => {
