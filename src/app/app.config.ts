7858a58d02356dcd9e81f178f0f53b650a8916fb
yanndarkz <iansantosmcqueen@gmail.com>
2024-11-04 23:29:32 -0300

    feature provider Auth0 app.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 import { provideHttpClient } from '@angular/common/http';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAuth0({
+    domain: 'dev-82ubfpsnq80d4hsz.us.auth0.com',
+    clientId: 'tJQiaQziikKvtbUCQtZGdYO2biLrXMP0',
+    authorizationParams: {
+      redirect_uri: window.location.origin,
+    },
+  }),]
 };
